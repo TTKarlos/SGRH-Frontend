85d05a40a7359d8b9d151a85ad782d93e2d93066
Karlos <carlossayubu@gmail.com>
2025-04-14 10:00:54 +0200

    [MOD] Cambiamos el vite.config para el servidor

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,27 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      'vue': path.resolve(__dirname, './node_modules/vue/dist/vue.esm-browser.js')
     }
   },
-  server: {
-    port: 8080,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        format: 'es',
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
       }
-    }
+    },
+    chunkSizeWarningLimit: 1000
   }
 });
